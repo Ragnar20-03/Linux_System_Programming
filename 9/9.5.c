b5d2f2551284c9df342a79256202729c1e955ede
Ragnar20-03 <roshanpp20@gmail.com>
2023-07-30 00:47:02 +0530

    9

--- 9/9.5.c
+++ 9/9.5.c
@@ -9,21 +9,8 @@
 int main(int argc  , char * argv[])
 {
 
-    int Ret = 0 ;
-    int RetX = 0 ;
-    printf("......\n");
-    Ret = fork();
-    if ( Ret == 0 )
-    {
-        execl ("./Demo" , "NULL" , NULL);
-    }
-    RetX = fork();
-    if ( Ret == 0 )
-    {
-        execl ("./DemoX" , "NULL" , NULL);
-    }
     sched_yield();
-// ///////////////
+
     return 0;
 
 }
