078b753b49bce813cfeff32f127759c6fd325559
Ragnar20-03 <roshanpp20@gmail.com>
2023-07-30 00:20:27 +0530

    9.7.c

--- 9/9.7.c
+++ 9/9.7.c
@@ -2,18 +2,25 @@
 #include<stdlib.h>
 #include<unistd.h>
 #include <sys/resource.h>
+#include<string.h>
 
 extern char **environ;
 
 int main()
 {
     char **env = environ;
-
+    char **envX = environ;
+    char * temp = NULL;
     printf("Environment of running process is : \n");
     
-    for(;*env != NULL; env++)
+    for(int iCnt = 0 ;*env != NULL; env++  )
     {
-        printf("%s : \n",*env);
+        temp = strtok(*env , "=");
+        if (( strcmp(temp , "HOME") ==  0) || ( strcmp(temp , "USER") ==  0))
+        {
+          temp = strtok(NULL , "=");
+          printf(" %s\n" , temp );
+        }
     }
     
     return 0;
